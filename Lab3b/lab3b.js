bde6a1fce0240030ac162373d796fa100a681f10
Elizabeth <lizareji23@gmail.com>
2018-05-26 00:16:37 -0400

    Added lab 3 part 2

--- Lab3b/lab3b.js
+++ Lab3b/lab3b.js
@@ -1,39 +1,87 @@
 "use strict";
 var map;
-var addresses;
+//if not initialized, it is undefined and is not recognized as arrays sp you can't push or pop
+var addresses = []; // defined empty array
 var mapMarkers = []; // defining an array of MapMarker object & initializing it
 var MapMarker = /** @class */ (function () {
     function MapMarker(address) {
         this.Address = address;
+        this.Coordinates = { lat: 0, lng: 0 };
     }
     ;
     return MapMarker;
 }());
 var Toronto = { lat: 43, lng: -79.38 };
 $.ajax({
-    type: 'POST',
-    url: 'AClocations.json',
+    url: './AClocations.json',
     dataType: 'json',
     success: function (data) {
-        console.log(data);
-        // for(let i of data){
-        //     console.log(i.address);
-        // }
-        addresses = data;
-        for (var _i = 0, addresses_1 = addresses; _i < addresses_1.length; _i++) {
-            var i = addresses_1[_i];
-            console.log(i.address);
-            // add map marker to array of map markers
-            var newMapMarker = new MapMarker(i.address); // using address constructor here
+        //data is an array of objects in this context
+        //we could assign it directly but it is assigned by reference. Hence it affects both simultaneously
+        //hence duplicating by for loop
+        for (var _i = 0, data_1 = data; _i < data_1.length; _i++) {
+            var cl = data_1[_i];
+            addresses.push(cl);
+        }
+        for (var _a = 0, addresses_1 = addresses; _a < addresses_1.length; _a++) {
+            var i = addresses_1[_a];
+            var newMapMarker = new MapMarker(i.address + ", Toronto, Canada");
             mapMarkers.push(newMapMarker);
         }
-        console.log(mapMarkers);
     }
 });
 function initMap() {
+    var geocoder = new google.maps.Geocoder();
     map = new google.maps.Map(document.getElementById("map"), {
         center: Toronto,
         zoom: 8
     });
+    //    addMarker(Toronto);
+    //    getLatLng("1 Yonge Street Toronto, Ontario, Canada");
+    for (var _i = 0, addresses_2 = addresses; _i < addresses_2.length; _i++) {
+        var cl = addresses_2[_i];
+        var latlng = { lat: cl.lat, lng: cl.lon };
+        var newMapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);
+    }
+    //let markersIndex: number = 0;
+    for (var i = 0; i < 10; i++) {
+        // setTimeout(() => {}, 1000);
+        setLatitudeLongitude(i);
+        // console.log(mapMarkers[i].Coordinates)
+        addMarker(mapMarkers[1].Coordinates);
+    }
+    function setLatitudeLongitude(markersIndex) {
+        //assigns lat and long for each map marker
+        mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);
+        //    setTimeout(() => {console.log(mapMarkers[markersIndex])}, 1000);
+        console.log(mapMarkers[markersIndex].Coordinates);
+    }
+    function getLatLng(address) {
+        geocoder.geocode({ address: address }, function (results, status) {
+            //console.log(status)
+            if (status === 'OK') {
+                console.log('Latitude : ' + results[0].geometry.location.lat());
+                console.log('Longitude : ' + results[0].geometry.location.lng());
+                var coord = {
+                    lat: results[0].geometry.location.lat(),
+                    lng: results[0].geometry.location.lng()
+                };
+                // console.log(coord);
+                return coord;
+            }
+            else {
+                console.log('In else');
+                setInterval(getLatLng(address), 2000);
+            }
+        });
+    }
+    function addMarker(coord) {
+        //will place mapmarker based on coordinates
+        var newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: "A cool place to be"
+        });
+    }
 }
-initMap();
--- Lab3b/lab3b.ts
+++ Lab3b/lab3b.ts
@@ -1,5 +1,6 @@
 let map: any;
-let addresses : any[];
+//if not initialized, it is undefined and is not recognized as arrays sp you can't push or pop
+let addresses : any[] = []; // defined empty array
 let mapMarkers : MapMarker[] = []; // defining an array of MapMarker object & initializing it
 
 // interface
@@ -10,42 +11,38 @@ interface LatLng {
 
 class MapMarker{
     Address: string;
-    LatLng: LatLng;
+    Coordinates : LatLng;
 
     public constructor(address: string){
       this.Address = address;
+      this.Coordinates = {lat: 0, lng: 0};
     };
 }
 
 let Toronto : LatLng = { lat: 43, lng: -79.38 };
 
 $.ajax({
-    type: 'POST',
-    url: 'AClocations.json',
+    url: './AClocations.json',
     dataType: 'json',
     success: function(data) {
-
-        console.log(data)
-        // for(let i of data){
-        //     console.log(i.address);
-        // }
-
-        addresses =  data;
-        for(let i of addresses){
-            console.log(i.address);
-
-            // add map marker to array of map markers
-            let newMapMarker : MapMarker = new MapMarker(i.address); // using address constructor here
+        //data is an array of objects in this context
+        //we could assign it directly but it is assigned by reference. Hence it affects both simultaneously
+        //hence duplicating by for loop
+        for(let cl of data) {
+            addresses.push(cl);
+        }
+        for(let i of addresses) {
+            let newMapMarker : MapMarker = new MapMarker(i.address + `, Toronto, Canada`);
 
             mapMarkers.push(newMapMarker);
         }
 
-        console.log(mapMarkers);
+        
     }
-
 });
 
 function initMap(){
+    let geocoder : object = new google.maps.Geocoder();
    map = new google.maps.Map(
      document.getElementById("map"),
      {
@@ -53,6 +50,65 @@ function initMap(){
          zoom: 8
      }
    );
-}
 
-initMap();
+//    addMarker(Toronto);
+//    getLatLng("1 Yonge Street Toronto, Ontario, Canada");
+
+   for(let cl of addresses) {
+        let latlng : LatLng = { lat: cl.lat, lng: cl.lon};
+        let newMapMarker : MapMarker = new MapMarker(cl.address);
+        mapMarkers.push(newMapMarker);
+   }
+
+   //let markersIndex: number = 0;
+    for(let i = 0; i < 10; i++) {
+        
+        // setTimeout(() => {}, 1000);
+        setLatitudeLongitude(i);
+        // console.log(mapMarkers[i].Coordinates)
+        addMarker(mapMarkers[1].Coordinates);
+    }
+    
+    function setLatitudeLongitude(markersIndex : number) : void {
+        //assigns lat and long for each map marker
+        mapMarkers[markersIndex].Coordinates = getLatLng(mapMarkers[markersIndex].Address);  
+    //    setTimeout(() => {console.log(mapMarkers[markersIndex])}, 1000);
+    console.log(mapMarkers[markersIndex].Coordinates)
+    }
+
+   function getLatLng(address : string) : LatLng {
+        
+        geocoder.geocode(
+            {address: address},
+            function(results, status) {
+                //console.log(status)
+                if(status === 'OK') {
+                    console.log('Latitude : ' + results[0].geometry.location.lat());
+                    console.log('Longitude : ' + results[0].geometry.location.lng());
+                    var coord = {
+                        lat: results[0].geometry.location.lat(), 
+                        lng: results[0].geometry.location.lng() 
+                    };
+                    // console.log(coord);
+                    return coord;
+                    
+                } else {
+                    console.log('In else');
+                    setInterval( getLatLng(address), 2000);
+                    
+                }
+            }
+        );
+
+   }
+
+   function addMarker(coord : LatLng) : void {
+        //will place mapmarker based on coordinates
+        
+        let newMarker = new google.maps.Marker({
+            position: coord,
+            map: map,
+            title: `A cool place to be`
+        });
+   }
+}
